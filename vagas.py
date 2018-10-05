61977e3826d9f72cd0632898cffea771793c6759
André <oioi-skin84@hotmail.com>
2018-10-05 14:10:00 -0300

    Update vagas.py

--- vagas.py
+++ vagas.py
@@ -119,9 +119,9 @@ def ftec_get_all_jobs():
     response.headers['Content-Type'] = 'application/json'
     response.headers['Cache-Control'] = 'no-cache'
 
-    #browser = Browser('firefox', executable_path='/home/andre/Python/ucs/geckodriver')
     #browser = Browser('firefox', executable_path='https://github.com/notvad/heroku-buildpack-selenium')
-    browser = Browser('firefox', executable_path='https://github.com/aoliveir1/trabalheconosco/blob/master/geckodriver')
+    #browser = Browser('firefox', executable_path='https://github.com/aoliveir1/trabalheconosco/blob/master/geckodriver')
+    browser = Browser()
     browser.visit('http://educacional.ftec.com.br:8080/RM/Rhu-BancoTalentos/#/RM/Rhu-BancoTalentos/painelVagas/lista')
     time.sleep(10)
     soup = BeautifulSoup(browser.html, 'html.parser')
