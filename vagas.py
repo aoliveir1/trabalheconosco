582211751e504ed84dc6ba3c4613ca5eb214954d
André <oioi-skin84@hotmail.com>
2019-03-18 10:01:50 -0300

    Update vagas.py

--- vagas.py
+++ vagas.py
@@ -219,6 +219,7 @@ jobs_flexxo2 = soup.find_all('div', {'class': 'oportunidade rounded last'})
 
 jobs_flexxo = []
 for job in zip(jobs_flexxo1, jobs_flexxo2):
+    print(job)
     soup = BeautifulSoup(str(job), 'html.parser')
     links = soup.find_all('a')
     for link in links:
