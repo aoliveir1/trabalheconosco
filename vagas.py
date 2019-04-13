00193fe55acad06d3821e058acd6e4b02d2c705b
André <oioi-skin84@hotmail.com>
2019-04-12 21:57:24 -0300

    Update vagas.py

--- vagas.py
+++ vagas.py
@@ -57,17 +57,20 @@ def ucs_get_description(job_ucs):
 @get('/jobs_ucs')
 def ucs_get_all_jobs():
     v_ucs=[]
+    try:
+        for job_ucs in jobs_ucs:
+            job_ucs = str(job_ucs)
+            d_ucs = {'vaga': ucs_get_job(job_ucs),
+                     'formacao': ucs_get_formation(job_ucs),
+                     'localidade': ucs_get_locale(job_ucs),
+                     'turno': ucs_get_turn(job_ucs),
+                     'descricao': ucs_get_description(job_ucs)}
+            v_ucs.append(d_ucs)
 
-    for job_ucs in jobs_ucs:
-        job_ucs = str(job_ucs)
-        d_ucs = {'vaga': ucs_get_job(job_ucs),
-                 'formacao': ucs_get_formation(job_ucs),
-                 'localidade': ucs_get_locale(job_ucs),
-                 'turno': ucs_get_turn(job_ucs),
-                 'descricao': ucs_get_description(job_ucs)}
-        v_ucs.append(d_ucs)
-
-    return json.dumps(v_ucs)
+        return json.dumps(v_ucs)
+    except:
+        print('erro em ucs')
+        return json.dumps(v_ucs)
 
 '''
 HG
@@ -128,18 +131,21 @@ def hg_get_requirements(job):
 @get('/jobs_hg')
 def hg_get_all_jobs():
     v_hg=[]
+    try:
+        for job in jobs:
+            job = str(job)
+            d_hg = {'vaga': hg_get_job(job),
+                 'setor': hg_get_sector(job),
+                 'carga horaria': hg_get_working_hours(job),
+                 'horario': hg_get_schedule(job),
+                 'contrato': hg_get_contract(job),
+                 'requisito': hg_get_requirements(job)}
+            v_hg.append(d_hg)
 
-    for job in jobs:
-        job = str(job)
-        d_hg = {'vaga': hg_get_job(job),
-             'setor': hg_get_sector(job),
-             'carga horaria': hg_get_working_hours(job),
-             'horario': hg_get_schedule(job),
-             'contrato': hg_get_contract(job),
-             'requisito': hg_get_requirements(job)}
-        v_hg.append(d_hg)
-
-    return json.dumps(v_hg)
+        return json.dumps(v_hg)
+    except:
+        print('erro em hg')
+        return json.dumps(v_hg)
 
 '''
 FTEC
@@ -212,22 +218,25 @@ Flexxo
 '''
 @get('/jobs_flexxo')
 def flexxo_get_all_jobs():
-    url = 'http://www.flexxo.com.br/Caxias+do+Sul/oportunidades/'
-    headers = {'User-Agent': 'Mozilla/5.0 (X11; Fedora; Linu…) Gecko/20100101 Firefox/65.0'.encode('utf-8')}
-    req = urllib.request.Request(url, headers=headers)
-    page = urllib.request.urlopen(req)
-    soup = BeautifulSoup(page, 'html.parser')
-    jobs_flexxo1 = soup.find_all('div', {'class': 'oportunidade rounded'})
-    jobs_flexxo2 = soup.find_all('div', {'class': 'oportunidade rounded last'})
-
     jobs_flexxo = []
-    for job in zip(jobs_flexxo1, jobs_flexxo2):
-        soup = BeautifulSoup(str(job), 'html.parser')
-        links = soup.find_all('a')
-        for link in links:
-            jobs_flexxo.append({'vaga': link.text.strip(), 'link': link['href']})
+    try:
+        url = 'http://www.flexxo.com.br/Caxias+do+Sul/oportunidades/'
+        headers = {'User-Agent': 'Mozilla/5.0 (X11; Fedora; Linu…) Gecko/20100101 Firefox/65.0'.encode('utf-8')}
+        req = urllib.request.Request(url, headers=headers)
+        page = urllib.request.urlopen(req)
+        soup = BeautifulSoup(page, 'html.parser')
+        jobs_flexxo1 = soup.find_all('div', {'class': 'oportunidade rounded'})
+        jobs_flexxo2 = soup.find_all('div', {'class': 'oportunidade rounded last'})
+        for job in zip(jobs_flexxo1, jobs_flexxo2):
+            soup = BeautifulSoup(str(job), 'html.parser')
+            links = soup.find_all('a')
+            for link in links:
+                jobs_flexxo.append({'vaga': link.text.strip(), 'link': link['href']})
 
-    return json.dumps(jobs_flexxo)
+        return json.dumps(jobs_flexxo)
+    except:
+        print('erro em flexxo')
+        return json.dumps(jobs_flexxo)
 
 '''
 Randon
@@ -235,20 +244,24 @@ Randon
 
 @get('/jobs_randon')
 def randon_get_all_jobs():
-    url = 'https://randon.gupy.io'
-    headers = {'User-Agent': 'Mozilla/5.0 (X11; Fedora; Linu…) Gecko/20100101 Firefox/65.0'.encode('utf-8')}
-    req = urllib.request.Request(url, headers=headers)
-    page = urllib.request.urlopen(req)
-    soup = BeautifulSoup(page, 'html.parser')
-    jobs_randon1 = soup.find_all('tr', {'data-workplace': 'Caxias do Sul'})
     v_randon = []
-    for job in jobs_randon1:
-        soup = BeautifulSoup(str(job), 'html.parser')
-        titulo = soup.find('span', {'class': 'title'})
-        url_vaga = url+job.a['href']
-        d_randon = {'vaga': str(titulo.text).strip(), 'link': url_vaga}
-        v_randon.append(d_randon)
-    return json.dumps(v_randon)
+    try:
+        url = 'https://randon.gupy.io'
+        headers = {'User-Agent': 'Mozilla/5.0 (X11; Fedora; Linu…) Gecko/20100101 Firefox/65.0'.encode('utf-8')}
+        req = urllib.request.Request(url, headers=headers)
+        page = urllib.request.urlopen(req)
+        soup = BeautifulSoup(page, 'html.parser')
+        jobs_randon1 = soup.find_all('tr', {'data-workplace': 'Caxias do Sul'})
+        for job in jobs_randon1:
+            soup = BeautifulSoup(str(job), 'html.parser')
+            titulo = soup.find('span', {'class': 'title'})
+            url_vaga = url+job.a['href']
+            d_randon = {'vaga': str(titulo.text).strip(), 'link': url_vaga}
+            v_randon.append(d_randon)
+        return json.dumps(v_randon)
+    except:
+        print('erro em randon')
+        return json.dumps(v_randon)
         
 '''
 Menon
@@ -256,6 +269,7 @@ Menon
 
 @get('/jobs_menon')
 def menon_get_all_jobs():
+    v_menon = []
     try:
         url='http://www.menonatacadista.com.br/trabalhe-conosco'
         headers = {'User-Agent': 'Mozilla/5.0 (X11; Fedora; Linu…) Gecko/20100101 Firefox/65.0'.encode('utf-8')}
@@ -265,19 +279,20 @@ def menon_get_all_jobs():
         container = soup.find_all('div', {'class': 'container'})
         soup = BeautifulSoup(str(container), 'html.parser')
         jobs = soup.find_all('p', {'style': 'text-align: justify; '})
-        v_menon = []
         for job in jobs:
             d_menon = {'vaga': job.text.strip()}
             v_menon.append(d_menon)
         return json.dumps(v_menon)
     except:
-        pass
+        print('erro em menon')
+        return json.dumps(v_menon)
     
 '''
 RBS
 '''
 @get('/jobs_rbs')
 def rbs_get_all_jobs():
+    jobs_rbs = []
     try:
         headers = {'User-Agent': 'Mozilla/5.0 (X11; Fedora; Linu…) Gecko/20100101 Firefox/65.0'.encode('utf-8')}
         url='http://www.gruporbs.com.br/talentosrbs/trabalhe-conosco/page/'
@@ -293,7 +308,6 @@ def rbs_get_all_jobs():
                 n += 1
             else:
                 break
-        jobs_rbs = []
         for jobs in all_jobs:
             for job in jobs:
                 soup = BeautifulSoup(str(job), 'html.parser')
@@ -317,7 +331,7 @@ def rbs_get_all_jobs():
                     jobs_rbs.append(d)
         return json.dumps(jobs_rbs)
     except:
-        pass
+        return json.dumps(jobs_rbs)
 
 '''
 Circulo
@@ -325,33 +339,36 @@ Circulo
 
 @get('/jobs_circulo')
 def circulo_get_all_jobs():
-    url = 'https://circulosaude.com.br/fale-conosco/trabalhe-conosco/'
-    headers = {'User-Agent': 'Mozilla/5.0 (X11; Fedora; Linu…) Gecko/20100101 Firefox/65.0'.encode('utf-8')}
-    req = urllib.request.Request(url, headers=headers)
-    page = urllib.request.urlopen(req)
-    soup = BeautifulSoup(page, 'html.parser')
-    jobs = soup.find_all('div', {'class': 'l-oportunidade'})
     jobs_circulo = []
-    for job in jobs:
-        soup = BeautifulSoup(str(job), 'html.parser')
-        vaga = soup.find('p', {'class': 'negrito'})
-        vaga = vaga.text
-        desc = soup.find('ul')
-        desc = str(desc.text)
-        pos1 = desc.find('Horário:')
-        pos2 = desc.find('Local:', pos1)
-        horario = (desc[pos1+8:pos2]).strip()
-        pos1 = desc.find('Requisitos:')
-        local = (desc[pos2+6:pos1]).strip()
-        pos2 = desc.find('Competências necessárias:')
-        requisitos = (desc[pos1+11:pos2]).strip()
-        competencias = (desc[pos2+len('Competências necessárias:'):]).strip()
-        d = {'vaga': vaga,
-             'horario': horario,
-             'requisitos': requisitos,
-             'competencias': competencias}
-        jobs_circulo.append(d)
-    return json.dumps(jobs_circulo)
+    try:
+        url = 'https://circulosaude.com.br/fale-conosco/trabalhe-conosco/'
+        headers = {'User-Agent': 'Mozilla/5.0 (X11; Fedora; Linu…) Gecko/20100101 Firefox/65.0'.encode('utf-8')}
+        req = urllib.request.Request(url, headers=headers)
+        page = urllib.request.urlopen(req)
+        soup = BeautifulSoup(page, 'html.parser')
+        jobs = soup.find_all('div', {'class': 'l-oportunidade'})
+        for job in jobs:
+            soup = BeautifulSoup(str(job), 'html.parser')
+            vaga = soup.find('p', {'class': 'negrito'})
+            vaga = vaga.text
+            desc = soup.find('ul')
+            desc = str(desc.text)
+            pos1 = desc.find('Horário:')
+            pos2 = desc.find('Local:', pos1)
+            horario = (desc[pos1+8:pos2]).strip()
+            pos1 = desc.find('Requisitos:')
+            local = (desc[pos2+6:pos1]).strip()
+            pos2 = desc.find('Competências necessárias:')
+            requisitos = (desc[pos1+11:pos2]).strip()
+            competencias = (desc[pos2+len('Competências necessárias:'):]).strip()
+            d = {'vaga': vaga,
+                 'horario': horario,
+                 'requisitos': requisitos,
+                 'competencias': competencias}
+            jobs_circulo.append(d)
+        return json.dumps(jobs_circulo)
+    except:
+        return json.dumps(jobs_circulo)
 
 '''
 Senac
@@ -359,105 +376,110 @@ Senac
 
 @get('/jobs_senac')
 def senac_get_all_jobs():
-    url = ' https://trabalheconosco.senacrs.com.br/vagas/em-processo-de-selecao'
-    headers = {'User-Agent': 'Mozilla/5.0 (X11; Fedora; Linu…) Gecko/20100101 Firefox/65.0'.encode('utf-8')}
-    req = urllib.request.Request(url, headers=headers)
-    page = urllib.request.urlopen(req)
-    soup = BeautifulSoup(page, 'html.parser')
-    jobs = soup.find('dl', {'class': 'vagas'})
-
-    links = []
-    if '<dt>Caxias do Sul</dt>' in str(jobs):
-        pos_start = str(jobs).find('<dt>Caxias do Sul</dt>')
-        pos_end = str(jobs).find('<dt>', pos_start+1)
-        jobs = str(jobs)[pos_start:pos_end]
-
-        soup = BeautifulSoup(str(jobs), 'html.parser')
-        jobs = soup.find_all('a')
-
-        url = 'https://trabalheconosco.senacrs.com.br'
-        for job in jobs:
-            links.append(url+job['href'])
-
     jobs_senac = []
-    for link in links:
-        req = urllib.request.Request(link, headers=headers)
+    try:
+        url = ' https://trabalheconosco.senacrs.com.br/vagas/em-processo-de-selecao'
+        headers = {'User-Agent': 'Mozilla/5.0 (X11; Fedora; Linu…) Gecko/20100101 Firefox/65.0'.encode('utf-8')}
+        req = urllib.request.Request(url, headers=headers)
         page = urllib.request.urlopen(req)
         soup = BeautifulSoup(page, 'html.parser')
-        detalhes = soup.find('dl', {'class': 'detalhes-vaga'})
-        soup = BeautifulSoup(str(detalhes), 'html.parser')
-        vaga = soup.find('dt')
-        desc = []
-        for v in vaga:
-            desc.append(str(v).strip())
-            break
-        dados = soup.find_all('div', {'class': 'dados'})
-        for dado in dados:
-            soup = BeautifulSoup(str(dado), 'html.parser')
-            span = soup.find('span')
-            desc.append(str(span.text).strip())
+        jobs = soup.find('dl', {'class': 'vagas'})
 
-        d_senac = {'vaga': desc[0],
-                   'unidade': desc[1],
-                   'area': desc[3],
-                   'formacao': desc[4],
-                   'requisitos': desc[5],
-                   'beneficios': desc[6],
-                   'publicado': desc[7]}
-        
-        jobs_senac.append(d_senac)
-        
-    return json.dumps(jobs_senac)
+        links = []
+        if '<dt>Caxias do Sul</dt>' in str(jobs):
+            pos_start = str(jobs).find('<dt>Caxias do Sul</dt>')
+            pos_end = str(jobs).find('<dt>', pos_start+1)
+            jobs = str(jobs)[pos_start:pos_end]
+
+            soup = BeautifulSoup(str(jobs), 'html.parser')
+            jobs = soup.find_all('a')
+
+            url = 'https://trabalheconosco.senacrs.com.br'
+            for job in jobs:
+                links.append(url+job['href'])
+
+        for link in links:
+            req = urllib.request.Request(link, headers=headers)
+            page = urllib.request.urlopen(req)
+            soup = BeautifulSoup(page, 'html.parser')
+            detalhes = soup.find('dl', {'class': 'detalhes-vaga'})
+            soup = BeautifulSoup(str(detalhes), 'html.parser')
+            vaga = soup.find('dt')
+            desc = []
+            for v in vaga:
+                desc.append(str(v).strip())
+                break
+            dados = soup.find_all('div', {'class': 'dados'})
+            for dado in dados:
+                soup = BeautifulSoup(str(dado), 'html.parser')
+                span = soup.find('span')
+                desc.append(str(span.text).strip())
+
+            d_senac = {'vaga': desc[0],
+                       'unidade': desc[1],
+                       'area': desc[3],
+                       'formacao': desc[4],
+                       'requisitos': desc[5],
+                       'beneficios': desc[6],
+                       'publicado': desc[7]}
+
+            jobs_senac.append(d_senac)
+
+        return json.dumps(jobs_senac)
+    except:
+        return json.dumps(jobs_senac)
         
 '''
 STV
 '''
 @get('/jobs_stv')
 def stv_get_all_jobs():
-    url = 'http://rh.stv.com.br/'
-    headers = {'User-Agent': 'Mozilla/5.0 (X11; Fedora; Linu…) Gecko/20100101 Firefox/65.0'.encode('utf-8')}
-    req = urllib.request.Request(url, headers=headers)
-    page = urllib.request.urlopen(req)
-    soup = BeautifulSoup(page, 'html.parser')
-    table = soup.find('table', {'class': 'table table-striped'})
-    soup = BeautifulSoup(str(table), 'html.parser')
-    tr = soup.find_all('tr')
-    links = []
-    for td in tr:
-        if 'Caxias do Sul' in str(td):
-            soup = BeautifulSoup(str(td), 'html.parser')
-            a = soup.find('a')
-            links.append((a.text, a['href']))
-
     jobs_stv = []
-    for link in links:
-        req = urllib.request.Request(link[1], headers=headers)
+    try:
+        url = 'http://rh.stv.com.br/'
+        headers = {'User-Agent': 'Mozilla/5.0 (X11; Fedora; Linu…) Gecko/20100101 Firefox/65.0'.encode('utf-8')}
+        req = urllib.request.Request(url, headers=headers)
         page = urllib.request.urlopen(req)
         soup = BeautifulSoup(page, 'html.parser')
-        vaga = soup.find('div', {'class': 'col-md-12'})
-        vaga = str(vaga.text).strip()
-        pos1 = vaga.find('Descrição:')
-        pos2 = vaga.find('Habilidades:')
-        descricao = (vaga[pos1+10:pos2]).strip()
-        pos1 = vaga.find('Localidade:', pos2)
-        habilidade = (vaga[pos2+12:pos1]).strip()
-        pos2 = vaga.find('Horário:')
-        localidade = (vaga[pos1+11:pos2]).strip()
-        pos1 = vaga.find('Forma de Contratação', pos2)
-        horario = (vaga[pos2+8:pos1]).strip()
-        pos2 = vaga.find('Benefícios:', pos1)
-        contrato = (vaga[pos1+20:pos2]).strip()
-        pos1 = vaga.find('Remuneração:', pos2)
-        beneficios = (vaga[pos2+11:pos1]).strip()
-        pos2 = vaga.find('Observação:', pos1)
-        remuneracao = (vaga[pos1+12:pos2]).strip()
-        pos1 = vaga.find('Candidatar-se', pos2)
-        observacao = (vaga[pos2+11:pos1]).strip()
-        d = {'vaga': link[0], 'descricao': descricao, 'habilidades': habilidade, 'localidade': localidade, 'horario': horario,
-             'contrato': contrato, 'beneficios': beneficios, 'remuneracao': remuneracao, 'observacao': observacao}
-        jobs_stv.append(d)
-        
-    return json.dumps(jobs_stv)
+        table = soup.find('table', {'class': 'table table-striped'})
+        soup = BeautifulSoup(str(table), 'html.parser')
+        tr = soup.find_all('tr')
+        links = []
+        for td in tr:
+            if 'Caxias do Sul' in str(td):
+                soup = BeautifulSoup(str(td), 'html.parser')
+                a = soup.find('a')
+                links.append((a.text, a['href']))
+        for link in links:
+            req = urllib.request.Request(link[1], headers=headers)
+            page = urllib.request.urlopen(req)
+            soup = BeautifulSoup(page, 'html.parser')
+            vaga = soup.find('div', {'class': 'col-md-12'})
+            vaga = str(vaga.text).strip()
+            pos1 = vaga.find('Descrição:')
+            pos2 = vaga.find('Habilidades:')
+            descricao = (vaga[pos1+10:pos2]).strip()
+            pos1 = vaga.find('Localidade:', pos2)
+            habilidade = (vaga[pos2+12:pos1]).strip()
+            pos2 = vaga.find('Horário:')
+            localidade = (vaga[pos1+11:pos2]).strip()
+            pos1 = vaga.find('Forma de Contratação', pos2)
+            horario = (vaga[pos2+8:pos1]).strip()
+            pos2 = vaga.find('Benefícios:', pos1)
+            contrato = (vaga[pos1+20:pos2]).strip()
+            pos1 = vaga.find('Remuneração:', pos2)
+            beneficios = (vaga[pos2+11:pos1]).strip()
+            pos2 = vaga.find('Observação:', pos1)
+            remuneracao = (vaga[pos1+12:pos2]).strip()
+            pos1 = vaga.find('Candidatar-se', pos2)
+            observacao = (vaga[pos2+11:pos1]).strip()
+            d = {'vaga': link[0], 'descricao': descricao, 'habilidades': habilidade, 'localidade': localidade, 'horario': horario,
+                 'contrato': contrato, 'beneficios': beneficios, 'remuneracao': remuneracao, 'observacao': observacao}
+            jobs_stv.append(d)
+
+        return json.dumps(jobs_stv)
+    except:
+        return json.dumps(jobs_stv)
 
 '''
 Anhanguera
@@ -465,17 +487,19 @@ Anhanguera
 
 @get('/jobs_anhanguera')
 def anhanguera_get_all_jobs():
-    api = 'https://eb.vagas.com.br/pesquisa-vagas/kroton.json?c[]=Caxias+do+Sul&div[]=40464&div[]=60086&div[]=60081&div[]=60082&div[]=60084&div[]=63697&div[]=60085&div[]=60083&div[]=47132&div[]=60110&div[]=60111&div[]=60112&div[]=60113&div[]=60114&div[]=63698&page=1&page_size=135&q=caxias+do+sul'
-    response = requests.get(api)
-    vagas = json.loads(response.text)
-
     jobs_anhanguera = []
-    for i in vagas['anuncios']:
-        if 'Anhanguera' in i['empresa']:
-            d = {'vaga':i['cargo'],
-                 'url': i['url']}
-            jobs_anhanguera.append(d)
-    return json.dumps(jobs_anhanguera)
+    try:
+        api = 'https://eb.vagas.com.br/pesquisa-vagas/kroton.json?c[]=Caxias+do+Sul&div[]=40464&div[]=60086&div[]=60081&div[]=60082&div[]=60084&div[]=63697&div[]=60085&div[]=60083&div[]=47132&div[]=60110&div[]=60111&div[]=60112&div[]=60113&div[]=60114&div[]=63698&page=1&page_size=135&q=caxias+do+sul'
+        response = requests.get(api)
+        vagas = json.loads(response.text)
+        for i in vagas['anuncios']:
+            if 'Anhanguera' in i['empresa']:
+                d = {'vaga':i['cargo'],
+                     'url': i['url']}
+                jobs_anhanguera.append(d)
+        return json.dumps(jobs_anhanguera)
+    except:
+        return json.dumps(jobs_anhanguera)
 
 
 run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
